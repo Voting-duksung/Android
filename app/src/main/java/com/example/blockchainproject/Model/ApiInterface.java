bbb752ac3b0e4aaa9dc70c2a9592f59ccbeb578e
제갈은 <jegal-eun@jegal-eun-ui-MacBook-Pro.local>
2021-08-11 15:34:47 +0900

    투표 완료 후 voteListActivity 돌아가기

--- app/src/main/java/com/example/blockchainproject/Model/ApiInterface.java
+++ app/src/main/java/com/example/blockchainproject/Model/ApiInterface.java
@@ -9,7 +9,19 @@ import retrofit2.http.Query;
 
 public interface ApiInterface {
     //    @PUT("/retrofit/put/")
+    //    @PUT("/retrofit/put/")
     @GET("/app/getStartedPlace")
-    Call<PlaceInfo> getPlaceid(@Query("placeid") String placeid);
+    Call<PlaceInfo> getPlaceid(@Query("placeid") String placeid, @Query("isStarted") String isStarted);
+
+//    @FormUrlEncoded
+//    @POST("/app/setVote")
+//    Call<Vote> postFunc(@Field("Placeid") String Placeid, @Field("candidateid") String candidateid,@Field("UserNumber") String UserNumber );
+
+
+    //    @FormUrlEncoded
+    @GET("/app/setVote")
+    Call<Vote> getVote(@Query("placeid") String placeid, @Query("candidateid") int candidateid,@Query("UserNumber") String UserNumber );
+
+
 }
 
